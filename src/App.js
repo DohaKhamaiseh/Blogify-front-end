651c12b2b4e2c49b80d55a56da3602b7f6d02044
DohaKhamaiseh <dohakhamaiseh1999@gmail.com>
2023-03-26 13:40:15 +0000

    render the news

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Switcher from './components/Switcher';
 
 export default function App() {
-  const [scrollPosition, setScrollPosition] = useState(0);
+  // const [scrollPosition, setScrollPosition] = useState(0);
 
-  useEffect(() => {
-    window.addEventListener('scroll', () => {
-      setScrollPosition(window.scrollY);
-    });
-  }, []);
+  // useEffect(() => {
+  //   window.addEventListener('scroll', () => {
+  //     setScrollPosition(window.scrollY);
+  //   });
+  // }, []);
 
   return (
     <Switcher />
