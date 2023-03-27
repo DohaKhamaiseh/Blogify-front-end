49df7a47e70f210901c7d218e579f4b6da8ee63d
mohammadAlghanim <ghanimmohammad11@gmail.com>
2023-03-27 12:09:37 +0000

    news.css and update on News.js

--- src/components/ApiComponents/News.js
+++ src/components/ApiComponents/News.js
@@ -1,96 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import { useEffect, useState } from "react";
+import './News.css';
+
 export default function News() {
-    const [newsArr, setNewsArr] = useState([]);
+  const [newsArr, setNewsArr] = useState([]);
+  const [readMore, setReadMore] = useState(Array(5).fill(false));
 
-    const sendReq = async () => {
-        //console.log(process.env.Deploy_URL);
-        const serverURL = `https://blog-post-back-end.vercel.app/topHeadlines`;
-        const response = await fetch(serverURL);
-        const data = await response.json();
-        setNewsArr(data);
+  const sendReq = async () => {
+    const serverURL = 'https://blog-post-back-end.vercel.app/topHeadlines';
+    const response = await fetch(serverURL);
+    const data = await response.json();
+    setNewsArr(data);
+  };
 
-    }
+  const toggleReadMore = (index) => {
+    setReadMore((prevState) => {
+      const newState = [...prevState];
+      newState[index] = !prevState[index];
+      return newState;
+    });
+  };
 
-    useEffect(() => {
-        sendReq();
-    }, [])
+  useEffect(() => {
+    sendReq();
+  }, []);
 
-    return (
-        <>
-            <Carousel>
-
-                <Carousel.Item  >
-                    <img
-                        className="d-block w-60"
-                        src={newsArr[0]?.urlToImage}
-                        alt={newsArr[0]?.title}
-                        style={{ backgroundPosition: 'cover' }}
-                    />
-                    <Carousel.Caption>
-                        <h3>{newsArr[0]?.title}</h3>
-                        <p>{newsArr[0]?.description}</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item >
-                    <img
-                        className="d-block w-60"
-                        src={newsArr[1]?.urlToImage}
-                        alt={newsArr[1]?.title}
-                        style={{ backgroundPosition: 'cover' }}
-                    />
-                    <Carousel.Caption>
-                        <h3>{newsArr[1]?.title}</h3>
-                        <p>{newsArr[1]?.description}</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item >
-                    <img
-                        className="d-block w-60"
-                        src={newsArr[2]?.urlToImage}
-                        alt={newsArr[2]?.title}
-                    />
-
-                    <Carousel.Caption>
-                        <h3>{newsArr[2]?.title}</h3>
-                        <p>
-                            {newsArr[2]?.description}
-                        </p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-
-                <Carousel.Item >
-                    <img
-                        className="d-block w-60"
-                        src={newsArr[3]?.urlToImage}
-                        alt={newsArr[3]?.title}
-                    />
-
-                    <Carousel.Caption>
-                        <h3>{newsArr[3]?.title}</h3>
-                        <p>
-                            {newsArr[3]?.description}
-                        </p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-
-                <Carousel.Item >
-                    <img
-                        className="d-block w-60"
-                        src={newsArr[4]?.urlToImage}
-                        alt={newsArr[4]?.title}
-                    />
-
-                    <Carousel.Caption>
-                        <h3>{newsArr[4]?.title}</h3>
-                        <p>
-                            {newsArr[4]?.description}
-                        </p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-
-        </>
-    )
+  return (
+    <>
+      <Carousel>
+        {newsArr.slice(8,15).map((item, index) => (
+          <Carousel.Item key={index}>
+            <img
+              className="d-block w-60"
+              src={item?.urlToImage}
+              alt={item?.title}
+           
+            />
+            <Carousel.Caption className="News-caption">
+              <h3>
+                {item?.title}
+                <button className='button read-more-button' onClick={() => toggleReadMore(index)}>
+                  {readMore[index] ? 'less' : 'more'}
+                </button>
+              </h3>
+              {readMore[index] ? (
+                  <p>{item?.description}</p>
+                  ) : (
+                  <p>{item?.content}</p>
+              )}
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </>
+  );
 }
+
